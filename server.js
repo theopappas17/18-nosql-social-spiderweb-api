7346355de02d9900a11b2296a5afc635a181b464
theopappas17 <theopappas17@gmail.com>
2022-07-24 22:09:10 -0400

    Did a lot of stuff

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-// Require model
-const { Department } = require('./models');
+const { User, Thought } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,57 +8,82 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Creates a new department
-app.post('/new-department/:department', (req, res) => {
-  // create the new department document
-  const newDepartment = new Department({ name: req.params.department });
-  // save it in the database
-  newDepartment.save();
-  if (newDepartment) {
-    res.status(201).json(newDepartment);
+app.post('/api/users', (req, res) => {
+  const newUser = new User({ name: req.params.user });
+  newUser.save();
+  if (newUser) {
+    res.status(201).json(newUser);
   } else {
-    console.log('Uh Oh, something went wrong');
+    console.log('Something went wrong');
     res.status(500).json({ error: 'Something went wrong' });
   }
 });
 
-// Finds all departments
-app.get('/all-departments', (req, res) => {
-  // Using model in route to find all documents that are instances of that model
-  Department.find({}, (err, result) => {
+app.post('/api/thoughts', (req, res) => {
+  const newThought = new Thought({ name: req.params.thought });
+  newThought.save();
+  if (newThought) {
+    res.status(201).json(newThought);
+  } else {
+    console.log('Something went wrong');
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+
+app.get('/users', (req, res) => {
+  User.find({}, (err, result) => {
     if (result) {
       res.status(200).json(result);
     } else {
-      console.log('Uh Oh, something went wrong');
+      console.log('Something went wrong');
       res.status(500).json({ error: 'Something went wrong' });
     }
   });
 });
 
-// Finds the first matching document
-app.get('/find-wine-department', (req, res) => {
-  // Using model in route to find all documents that are instances of that model
-  Department.findOne({ name: 'Produce' }, (err, result) => {
+app.get('/thoughts', (req, res) => {
+  Thought.find({}, (err, result) => {
     if (result) {
       res.status(200).json(result);
     } else {
-      console.log('Uh Oh, something went wrong');
+      console.log('Something went wrong');
+      res.status(500).json({ error: 'Something went wrong' });
+    }
+  })
+})
+
+app.get('/users/:user_id', (req, res) => {
+  User.findOne({ name: 'User' }, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log('Something went wrong');
       res.status(500).json({ error: 'Something went wrong' });
     }
   });
 });
 
-// Finds first document matching parameter and deletes
-// For demo, use 'Wine' as URL param
-app.delete('/find-one-delete/:departmentName', (req, res) => {
-  Department.findOneAndDelete(
-    { name: req.params.departmentName },
+app.get('/thoughts/:thougt_id', (req, res) => {
+  Thought.findOne({ name: 'Thought' }, (err, result) => {
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      console.log('Something went wrong');
+      res.status(500).json({ error: 'Something went wrong' });
+    }
+  });
+});
+
+app.delete('/user/:user_id', (req, res) => {
+  User.findOneAndDelete(
+    { name: req.params.user },
     (err, result) => {
       if (result) {
         res.status(200).json(result);
         console.log(`Deleted: ${result}`);
       } else {
-        console.log('Uh Oh, something went wrong');
+        console.log('Something went wrong');
         res.status(500).json({ error: 'Something went wrong' });
       }
     }
